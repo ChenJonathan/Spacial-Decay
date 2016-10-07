3e8cf154eeff624216a1dffa84871d9bab316d1d
Jonathan Chen <jonathanchen@gatech.edu>
2016-10-07 16:12:53 -0400

    Added difficulty select and Sean's level select art

--- Assets/Scripts/UI/Scroll.cs
+++ Assets/Scripts/UI/Scroll.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 /// <summary>
 /// Scroll bars that scroll through the level select vertically when moused over.
 /// </summary>
-public class Scroll : MonoBehaviour
+public class Scroll : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     // Scroll direction
     public bool Up;
 
     private Camera mainCamera;
+    private bool mouseOver;
     private float currentCameraSpeed;
 
     private readonly float CAMERA_START_SPEED = 0.2f;
@@ -21,29 +23,42 @@ public class Scroll : MonoBehaviour
     public void Awake()
     {
         mainCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        mouseOver = false;
         currentCameraSpeed = CAMERA_START_SPEED;
+    }
+
+    /// <summary>
+    /// Called periodically. Shifts the camera and increases the scroll speed over time if moused over.
+    /// </summary>
+    public void Update()
+    {
+        if(mouseOver)
+        {
+            mainCamera.transform.position = new Vector2(mainCamera.transform.position.x, mainCamera.transform.position.y + (Up ? currentCameraSpeed : -currentCameraSpeed));
+
+            if(mainCamera.transform.position.y < -CAMERA_MAX_Y)
+                mainCamera.transform.position = new Vector2(mainCamera.transform.position.x, -CAMERA_MAX_Y);
+            else if(mainCamera.transform.position.y > CAMERA_MAX_Y)
+                mainCamera.transform.position = new Vector2(mainCamera.transform.position.x, CAMERA_MAX_Y);
+
+            currentCameraSpeed += CAMERA_ACCELERATION * Time.deltaTime;
+        }
     }
 
     /// <summary>
     /// Called when the mouse first hovers over the scroll bar. Resets the scroll speed.
     /// </summary>
-    public void OnMouseEnter()
+    public void OnPointerEnter(PointerEventData eventData)
     {
+        mouseOver = true;
         currentCameraSpeed = CAMERA_START_SPEED;
     }
 
     /// <summary>
-    /// Called repeatedly when the mouse remains over the scroll bar. Shifts the camera and increases the scroll speed over time.
+    /// Called when the mouse leaves the scroll bar.
     /// </summary>
-    public void OnMouseOver()
+    public void OnPointerExit(PointerEventData eventData)
     {
-        mainCamera.transform.position = new Vector2(mainCamera.transform.position.x, mainCamera.transform.position.y + (Up ? currentCameraSpeed : -currentCameraSpeed));
-        
-        if(mainCamera.transform.position.y < -CAMERA_MAX_Y)
-            mainCamera.transform.position = new Vector2(mainCamera.transform.position.x, -CAMERA_MAX_Y);
-        else if(mainCamera.transform.position.y > CAMERA_MAX_Y)
-            mainCamera.transform.position = new Vector2(mainCamera.transform.position.x, CAMERA_MAX_Y);
-
-        currentCameraSpeed += CAMERA_ACCELERATION * Time.deltaTime;
+        mouseOver = false;
     }
 }
