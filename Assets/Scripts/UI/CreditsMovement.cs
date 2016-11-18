c1acc982eaa8473353b89dd34d242e2d2d07db55
Christian Girala <christiangirala@gmail.com>
2016-11-18 17:36:51 -0500

    put in credits txt
    created script to move image

---
+++ Assets/Scripts/UI/CreditsMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsMovement : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject text;
+
+    private Transform start, end;
+
+	// Use this for initialization
+	void Start () {
+        start = new Vector3(36,-430,0);
+        end = new Vector3(36, 430, 0);
+       
+	}
+
+    public void Move()
+    {
+        text.transform.position = Vector3.Lerp(start, end, 0.2f);
+    }
+
+    public void Stop()
+    {
+        text.transform.position = new Vector3(text.transform.position.x, text.transform.position.y, text.transform.position.z);
+    }
+}
